d658a8e791402742ee9e80f9784eea3e0946650b
Jude Taylor <me@jude.bio>
2018-02-21 16:12:31 -0800

    fix Travis build, add GHC 7.6.3, remove hlint test

--- tests/hlint.hs
+++
@@ -1,6 +0,0 @@
-import Control.Monad
-import Language.Haskell.HLint
-import System.Exit
-
-main :: IO ()
-main = hlint [".", "--cpp-define=TEST_HACKAGE=1"] >>= \ hints -> unless (null hints) exitFailure
