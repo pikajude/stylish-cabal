8bfad45db890d5f40bb1c8744a31c651c0b15d12
Jude Taylor <me@jude.xyz>
2018-07-10 00:46:48 -0700

    fix mixin rendering

--- default.nix
+++
@@ -1,9 +0,0 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc802" }:
-
-with nixpkgs.pkgs; stdenv.mkDerivation {
-  name = "shellenv";
-  buildInputs = [
-    zlib
-    haskell.compiler."${compiler}"
-  ];
-}
